0dbeeaa2cff206d5b569d79f60749d4bf0e81d7a
Peter <peterwang998@gmail.com>
2018-07-17 09:04:54 -0700

    Updating video processing scripts

--- createClips/createClips.py
+++ createClips/createClips.py
@@ -8,6 +8,7 @@
 # Purpose: Create video clips based on the time markers provides in a CSV file.
 # Settings: clip time, participant name
 
+import glob
 import os, sys
 
 # CONSTANTS
@@ -15,42 +16,62 @@ CUT_TIME = 0
 TIME = 0
 
 # FUNCTIOINS
-def create_clip(cut_in, cut_out):
+def create_clip(cut_in, cut_out, timestamp, out_dir):
     print("Cutting from {} to {}".format(cut_in, cut_out))
     # run an ffmpeg thread to create MP4 clips
     mp4 = 'ffmpeg -i ' + videoFile + ' -ss ' + \
-        cut_in + \
+        str(cut_in) + \
         ' -to ' + \
-        cut_out + \
-        ' -c copy clip' + str(clip_counter) + '.mp4'
+        str(cut_out) + \
+        ' -c copy {}{}_{}.mov'.format(out_dir, participantNumber, timestamp)
     print(mp4)
     os.system(mp4)
 
 # SCRIPT
 # Check for the command line inputs
-if len(sys.argv) < 5:
-    print("No video file or cut file given\nUsage: \
-    python clipCreator.py [videoFile] [cutFile] [seconds] [startTime]")
+if len(sys.argv) < 3:
+    print("No participant directory given\nUsage: \
+    python clipCreator.py [participantDirectory] [seconds]")
     quit()
 else:
-    videoFile = sys.argv[1]
-    cutFile = sys.argv[2]
-    cutSeconds = int(sys.argv[3])
-    startTime = float(sys.argv[4])
+    participantDirectory = sys.argv[1]
+    cutSeconds = float(sys.argv[2])
+    os.chdir(participantDirectory) # [2]
+    print("Moving into {}".format(os.getcwd()))
 
 
+# Get the files we need [1]
+videoFile = glob.glob("*.mov")[0]
+cutFile = glob.glob("*.csv")[0]
+startFile = glob.glob("*.txt")[0]
+participantNumber = participantDirectory.split('/')[-1] #last element of list is the participantNumber
+print(participantNumber)
 print("Cutting {} into {} second clips using {}".format(videoFile, cutSeconds, cutFile))
+os.mkdir('{}_no-response'.format(participantNumber))
+os.mkdir('{}_response'.format(participantNumber))
+
+# Get the start time from the second line of the file
+with open(startFile, 'r') as f:
+    for line in f:
+        x = line
+    startTime = float(x.split(': ')[-1])
+    print("startime = {}".format(startTime))
 
 # Find all the cut points and create the clips
 linenum = 0
-clip_counter = 0
 with open(cutFile, 'r') as f:
     for line in f:
         line = line.strip().split(",")
-        print(line)
+        #print(line)
         if linenum != 0:
-            question_time = float(line[TIME]) - startTime
+            timestamp = float(line[TIME])
+            question_time = timestamp - startTime
             cut_time = question_time - cutSeconds
-            print(cut_time, question_time)
-            create_clip(cut_time, question_time)
+            #print(cut_time, question_time)
+            create_clip(cut_time, question_time, timestamp, '{}_no-response/'.format(participantNumber)) # clipped up to question
+            create_clip(cut_time, question_time + 5, timestamp, '{}_response/'.format(participantNumber)) # clipped 5 seconds after the questions is asked
         linenum += 1
+
+# REFERENCES
+# [1] Getting filenames using glob: https://stackoverflow.com/questions/3964681/find-all-files-in-a-directory-with-extension-txt-in-python
+# Changing working directory: https://stackoverflow.com/questions/17359698/how-to-get-the-current-working-directory-using-python-3/17361545
